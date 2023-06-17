39970ec7e21044fc39dd334998bd13846f508a96
Yehia <yehia.abdelazziz@tum.de>
2023-06-17 14:25:31 +0300

    Added the login/logout route, adjusted the Navbar, required folder structure to be made

--- app.py
+++ app.py
@@ -20,10 +20,9 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
-def login():
+@app.route('/')
+def index():
     return render_template("index.html")
-
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
@@ -68,7 +67,42 @@ def register():
         if __name__ == '__main__':
           app.run(port=5000,debug=True, use_reloader=True)
 
-        return redirect("/")
+        return redirect("/login")
+    
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+      username = request.form.get("username")
+      password = request.form.get("password")
+    
+      if username == "" or password == "":
+          flash("Provide username or password")
+          return redirect("/login")
+      
+      userdata = db.execute("SELECT * FROM users WHERE username = ?", username)
+      if len(userdata) == 0:
+          flash("Username is incorrect")
+          return redirect("/login")
+      
+      password_hash = userdata[0]["PWhash"]
+      if check_password_hash(password_hash, password) is False:
+          flash("wrong password")
+          return redirect("/login")
+      else:
+          session["userid"] = userdata[0]["id"]
+          flash("Successful login!")
+          return redirect("/")
+      
+    else:
+        return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/login")
+    
     
 
 
